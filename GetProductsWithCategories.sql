b3c6532111aa169e78b817d01f3473a7a3adf9ca
Alyona <alianakass@icloud.com>
2022-09-20 17:36:01 +0300

    Add project files.

---
+++ GetProductsWithCategories.sql
@@ -0,0 +1,3 @@
+﻿select P.Name as 'ProductName', C.Name as 'CategoryName' from Products as P 
+left join Category_Product as CP on P.Id = CP.ProductId 
+left join Categories as C on C.Id = CP.CategoryId
